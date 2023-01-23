05ab692c5426a28551fca799b2cb2e0f0bfd8c68
Keshikaravi <keshikaravi2000@gmail.com>
2023-01-23 13:00:05 +0530

    added routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import logo from "./logo.svg";
 import "./App.css";
-//import TodoList from "./components/Todolist";
+import { Routes, Route } from "react-router-dom";
+import TodoList from "./components/Todolist";
 import LoginForm from "./components/LoginForm";
 import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
-    <div className="App">
-      <LoginForm />
-      {/* <TodoList /> */}
-    </div>
+    <Routes>
+      <Route path="/" element={<LoginForm />}></Route>
+
+      <Route path="Todolist" element={<TodoList />}></Route>
+    </Routes>
   );
 }
 
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,7 +1,10 @@
 import React from "react";
 import "./loginForm.css";
-
+import { useNavigate } from "react-router-dom";
 const LoginForm = () => {
+  //   let history = useHistory();
+  //   history.push("/TodoList");
+  const navigate = useNavigate();
   return (
     <div className="page">
       <div className="cover">
@@ -9,7 +12,9 @@ const LoginForm = () => {
         <input type="text" placeholder="username" />
         <input type="password" placeholder="password" />
         {/* <button className="button">Login</button> */}
-        <div className="login-btn">Login</div>
+        <div className="login-btn" onClick={() => navigate("TodoList")}>
+          Login
+        </div>
         {/* <p className="text">Forgot password</p>
         <div className="alt-login">
           <div>Reset</div>{" "}
